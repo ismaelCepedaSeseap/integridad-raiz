45db287242a9e641c77470cb48bb97d11c20e76f
Ismael Habacuq Cepeda Morales <ismael.cepeda@seseap.puebla.gob.mx>
2026-02-13 16:42:26 -0600

    fix_absolute_paths_php

--- assets/php/admin/obtenerUsuarios.php
+++ assets/php/admin/obtenerUsuarios.php
@@ -1,7 +1,7 @@
 <?php
     // session_start();
-    require_once("../conexion.php");
-    require_once("../security/auth.php");
+    require_once __DIR__ . "/../conexion.php";
+    require_once __DIR__ . "/../security/auth.php";
 
     $autorizacion = new auth();
     // $autorizado = $autorizacion->isLoggedIn();
--- assets/php/security/initSession.php
+++ assets/php/security/initSession.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once 'auth.php';
+    require_once __DIR__ . '/auth.php';
     $auth = new Auth();
     if($actual!="login.php"){
         if(!$auth->isLoggedIn()){
